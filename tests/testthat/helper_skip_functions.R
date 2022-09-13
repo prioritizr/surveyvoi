72bfc90dc0befbaa2cb6aa0eb47e2887880e7031
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2022-09-13 13:26:14 -0400

    double check fixed fedora

--- tests/testthat/helper_skip_functions.R
+++ tests/testthat/helper_skip_functions.R
@@ -8,14 +8,15 @@ skip_if_parallel_processing_not_available <- function() {
 }
 
 skip_on_fedora <- function() {
-  os_name <- utils::sessionInfo()$running
-  is_fedora <- TRUE
-  if (
-    is.character(os_name) &&
-    identical(length(os_name), 1L) &&
-    all(!is.na(os_name))
-  ) {
-    is_fedora <- any(grepl("fedora", tolower(os_name), fixed = TRUE))
-  }
-  skip_if(isTRUE(is_fedora), message = "on Fedora")
+  TRUE
+  # os_name <- utils::sessionInfo()$running
+  # is_fedora <- TRUE
+  # if (
+  #   is.character(os_name) &&
+  #   identical(length(os_name), 1L) &&
+  #   all(!is.na(os_name))
+  # ) {
+  #   is_fedora <- any(grepl("fedora", tolower(os_name), fixed = TRUE))
+  # }
+  # skip_if(isTRUE(is_fedora), message = "on Fedora")
 }
